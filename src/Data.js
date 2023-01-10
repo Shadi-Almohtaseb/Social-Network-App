13924c252735c8ff488609ece4dcd5cf05a610e5
Shadi-Almohtaseb <shadi876543211@gmail.com>
2023-01-10 19:31:51 +0200

    Josn API

---
+++ src/Data.js
@@ -0,0 +1,332 @@
+const newDataAPI = [
+  {
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    author: "Rob McLean",
+    title:
+      "Fisher-Price recalls Rock 'n Play Sleepers again after 8 more deaths - CNN",
+    description:
+      "Fisher-Price for a second time recalled its Rock 'n Play Sleepers on Monday after at least eight infant deaths occurred after the initial 2019 recall, according to the Consumer Product Safety Commission.",
+    url: "https://www.cnn.com/2023/01/09/business/fisher-price-recall/index.html",
+    urlToImage:
+      "https://media.cnn.com/api/v1/images/stellar/prod/230109205307-rock-n-play-sleeper-recall-010923.jpg?c=16x9&q=w_800,c_fill",
+    publishedAt: "2023-01-10T16:08:00Z",
+    content:
+      "Fisher-Price for a second time recalled its Rock n Play Sleepers on Monday after at least eight infant deaths occurred after the initial 2019 recall, according to the Consumer Product Safety Commissi… [+1693 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "The News Journal",
+    },
+    author: ", The News Journal",
+    title:
+      "Bed Bath & Beyond to close Delaware Brandywine Town Center store - The News Journal",
+    description:
+      'In a public filing Thursday, Bed Bath & Beyond said it faces "substantial doubt" that it can continue operations.',
+    url: "https://www.delawareonline.com/story/money/business/2023/01/09/bed-bath-beyond-close-delaware-brandywine-town-center-retail-shopping-sales/69781975007/",
+    urlToImage:
+      "https://www.gannett-cdn.com/presto/2023/01/06/PWIL/103d1a05-2257-4f95-b0c0-93bbf56debca-010623-brandywine_town_center_-_bed_bath_beyond-wb049800.jpg?auto=webp&crop=3742,2105,x0,y0&format=pjpg&width=1200",
+    publishedAt: "2023-01-10T15:59:16Z",
+    content:
+      "As Bed Bath &amp; Beyond warns it could soon file for bankruptcy, at least one of the home goods retailer's locations in Delaware is slated to close.\r\nClosing sales will unfold \"over the next few mon… [+2280 chars]",
+  },
+  {
+    source: {
+      id: "bloomberg",
+      name: "Bloomberg",
+    },
+    author: null,
+    title:
+      "FTC Is Investigating Coke, Pepsi Price Discounting Under Antitrust Law - Bloomberg",
+    description: null,
+    url: "https://www.bloomberg.com/tosv2.html?vid=&uuid=eb7e7c74-9107-11ed-bbc3-585171777149&url=L25ld3MvYXJ0aWNsZXMvMjAyMy0wMS0xMC9mdGMtcHJvYmluZy1jb2tlLXBlcHNpLXByaWNlLWRpc2NvdW50aW5nLXVuZGVyLWFudGl0cnVzdC1sYXc=",
+    urlToImage: null,
+    publishedAt: "2023-01-10T15:59:11Z",
+    content:
+      "To continue, please click the box below to let us know you're not a robot.",
+  },
+  {
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    author: "Jordan Valinsky",
+    title:
+      "Disney CEO Bob Iger orders workers to return to the office 4 days a week - CNN",
+    description: "Disney CEO Bob Iger is clamping down on remote workers.",
+    url: "https://www.cnn.com/2023/01/10/business/disney-return-to-work/index.html",
+    urlToImage:
+      "https://media.cnn.com/api/v1/images/stellar/prod/230110083841-bob-iger-1129-file.jpg?c=16x9&q=w_800,c_fill",
+    publishedAt: "2023-01-10T15:24:00Z",
+    content:
+      "Disney CEO Bob Iger is clamping down on remote workers.\r\nIger, who recently returned to Disneys helm after a brief hiatus, ordered employees to return to corporate offices four days a week beginning … [+1940 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "Motley Fool",
+    },
+    author: "Jeremy Bowman",
+    title:
+      "Is It Time To Buy the S&P 500's 4 Worst-Performing Stocks? - The Motley Fool",
+    description: "There are plenty of bargains available in the market now.",
+    url: "https://www.fool.com/investing/2023/01/10/is-it-time-to-buy-the-sp-500s-4-worst-performing-s/",
+    urlToImage:
+      "https://g.foolcdn.com/editorial/images/715679/economic-crisis-stock-chart-falling-down-business-global-money-bankruptcy.jpg",
+    publishedAt: "2023-01-10T15:15:00Z",
+    content:
+      "For the S&amp;P 500, 2022 was the worst year since 2008 as the broad market index fell 19.4%. While no one likes to see their portfolio decline, savvy investors know that bear markets offer good buyi… [+3784 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "Investor's Business Daily",
+    },
+    author: "Investor's Business Daily",
+    title:
+      "Dow Jones Rises On Powell Comments; Boeing Slides On Downgrade - Investor's Business Daily",
+    description:
+      "Earnings reporters include Jefferies, Albertsons and Bed Bath & Beyond.",
+    url: "https://www.investors.com/market-trend/stock-market-today/dow-jones-futures-fall-ahead-of-powell-speech-boeing-slides-on-downgrade/",
+    urlToImage:
+      "https://www.investors.com/wp-content/uploads/2018/10/stock-Jerome-Powell-02-fed.jpg",
+    publishedAt: "2023-01-10T15:02:00Z",
+    content:
+      "The Dow Jones Industrial Average rose Tuesday amid Fed Chair Jerome Powell's comments. Meanwhile, aerospace giant Boeing (BA) slid after a Morgan Stanley downgrade.\r\nXPowell Comments, Earnings Result… [+8311 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "Fox Business",
+    },
+    author: "Kristen Altus",
+    title:
+      "Jamie Dimon sounds alarm on rising US debt having 'potentially disastrous outcomes' - Fox Business",
+    description:
+      'JPMorgan Chase CEO Jamie Dimon calls out the "dangerous" government debt and "shenanigans of politics" impacting sectors which could help the U.S. economy grow by 3%.',
+    url: "https://www.foxbusiness.com/economy/jamie-dimon-sounds-alarm-rising-us-debt-potentially-disastrous-outcomes",
+    urlToImage:
+      "https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2023/01/0/0/JAMIE-DIMON-JPMORGAN-GETTY.jpg?ve=1&tl=1",
+    publishedAt: "2023-01-10T14:37:31Z",
+    content:
+      'JPMorgan Chase CEO Jamie Dimon weighed in on fiscal policy under a new Congress and voiced concerns around rising debt’s macroeconomic impact in an exclusive four-part interview that aired on "Mornin… [+4682 chars]',
+  },
+  {
+    source: {
+      id: "bloomberg",
+      name: "Bloomberg",
+    },
+    author: null,
+    title:
+      "Microsoft Mulls $10 Billion Investment in ChatGPT Creator - Bloomberg",
+    description: null,
+    url: "https://www.bloomberg.com/tosv2.html?vid=&uuid=e9df1f0a-9107-11ed-ad36-554b78614158&url=L25ld3MvYXJ0aWNsZXMvMjAyMy0wMS0xMC9taWNyb3NvZnQtd2VpZ2hzLTEwLWJpbGxpb24tY2hhdGdwdC1pbnZlc3RtZW50LXNlbWFmb3Itc2F5cw==",
+    urlToImage: null,
+    publishedAt: "2023-01-10T14:36:07Z",
+    content:
+      "To continue, please click the box below to let us know you're not a robot.",
+  },
+  {
+    source: {
+      id: "business-insider",
+      name: "Business Insider",
+    },
+    author: null,
+    title:
+      "A brewery CEO says he had to pay out over $500000 to competition winners who found 'solid gold' beer cans that were actually just gold-plated - msnNOW",
+    description:
+      'Scottish beer firm Brewdog launched a "Willy Wonka" inspired competition which included hiding gold-plated cans in beer cases for customers to find.',
+    url: "https://www.businessinsider.com/brewery-ceo-says-paid-over-500000-to-customers-over-misleading-ads-2023-1",
+    urlToImage:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA16aUpi.img?h=630&w=1200&m=6&q=60&o=t&l=f&f=jpg",
+    publishedAt: "2023-01-10T14:33:24Z",
+    content:
+      "© Tang/Getty Images\r\n Brewdog's CEO said he paid over $500,000 to competition winners after wrongly saying they'd receive \"solid gold\" beer cans. Tang/Getty Images\r\nA Scottish brewery CEO said he pai… [+5096 chars]",
+  },
+  {
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    author: "Julia Horowitz",
+    title:
+      "Two global recessions in one decade? That hasn't happened in over 80 years - CNN",
+    description:
+      "The global economy is just one more knock away from a second recession in the same decade, something that hasn't happened in more than 80 years.",
+    url: "https://www.cnn.com/2023/01/10/economy/world-bank-recession-economy/index.html",
+    urlToImage:
+      "https://media.cnn.com/api/v1/images/stellar/prod/230110111916-federal-reserve-file-123022.jpg?c=16x9&q=w_800,c_fill",
+    publishedAt: "2023-01-10T14:32:00Z",
+    content:
+      "The global economy is just one more knock away from a second recession in the same decade, something that hasnt happened in more than 80 years.\r\nThats the latest warning from the World Bank, which on… [+2378 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "YouTube",
+    },
+    author: null,
+    title:
+      "Tesla shares are undervalued, says Loup's Munster - CNBC Television",
+    description:
+      "Gene Munster, Loup founder and managing partner, joins 'Squawk Box' to discuss the outlook for Tesla and other tech stocks. For access to live and exclusive ...",
+    url: "https://www.youtube.com/watch?v=HXTOir95nOc",
+    urlToImage: "https://i.ytimg.com/vi/HXTOir95nOc/maxresdefault.jpg",
+    publishedAt: "2023-01-10T14:30:05Z",
+    content: null,
+  },
+  {
+    source: {
+      id: null,
+      name: "Gizmodo.com",
+    },
+    author: "Lauren Leffer",
+    title:
+      "The Feds Are Investigating an Elon Musk Tweet About Full Self-Driving Update - Gizmodo",
+    description:
+      "In a reply to a fan, the billionaire seemed to suggest that Tesla drivers don't need to be reminded to keep their hands on the steering wheel.",
+    url: "https://gizmodo.com/feds-investigate-elon-musk-tweet-tesla-self-driving-1849966761",
+    urlToImage:
+      "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/29e3e851242ac496db5b127a7ca478c7.jpg",
+    publishedAt: "2023-01-10T14:10:00Z",
+    content:
+      "One of Elon Musks numerous tweets is now part of an ongoing federal investigation into the safety of Teslas automated driver-assistant system. The National Highway Traffic Safety Administration reque… [+5042 chars]",
+  },
+  {
+    source: {
+      id: "financial-times",
+      name: "Financial Times",
+    },
+    author: "Colby Smith",
+    title:
+      "Fed will not become a 'climate policymaker', Jay Powell says - Financial Times",
+    description:
+      "News, analysis and comment from the Financial Times, the worldʼs leading global business publication",
+    url: "https://www.ft.com/content/6abb5562-59a0-49a7-8cc0-8fb48e5d6fe9",
+    urlToImage: null,
+    publishedAt: "2023-01-10T14:01:02Z",
+    content:
+      "What is included in my trial?\r\nDuring your trial you will have complete digital access to FT.com with everything in both of our Standard Digital and Premium Digital packages.\r\nStandard Digital includ… [+1496 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "Bitcoin.com",
+    },
+    author: "Eliman Dambell",
+    title:
+      "Bitcoin, Ethereum Technical Analysis: ETH Above $1330, Consolidating Recent Gains – Market Updates Bitcoin News - Bitcoin News",
+    description:
+      "Ethereum mostly consolidated during Tuesday’s session, as prices continued to trade close to recent highs.",
+    url: "https://news.bitcoin.com/bitcoin-ethereum-technical-analysis-eth-above-1330-consolidating-recent-gains/",
+    urlToImage:
+      "https://static.news.bitcoin.com/wp-content/uploads/2023/01/shutterstock_1979755163.jpg",
+    publishedAt: "2023-01-10T13:01:55Z",
+    content:
+      "Ethereum mostly consolidated during Tuesday’s session, as prices continued to trade close to recent highs. Following a move to a three-week peak to start the week, markets maintained bullish sentimen… [+2793 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "CNBC",
+    },
+    author: "Jessica Dickler",
+    title:
+      "Americans lean more on credit cards as expenses stay high: 46% of cardholders now carry debt from month to month - CNBC",
+    description:
+      "More Americans carry credit card debt from month to month as living expenses stay high, according to a new report by Bankrate.",
+    url: "https://www.cnbc.com/2023/01/10/americans-lean-heavily-on-credit-cards-amid-inflation.html",
+    urlToImage:
+      "https://image.cnbcfm.com/api/v1/image/107142371-1666951593013-gettyimages-1371070653-dsc09376.jpeg?v=1673355601&w=1920&h=1080",
+    publishedAt: "2023-01-10T13:00:01Z",
+    content:
+      "With day-to-day expenses staying high due to inflation, more Americans are relying on credit cards to make ends meet.\r\nAs the personal savings rate sank near an all-time low, credit card balances jum… [+2960 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "CNBC",
+    },
+    author: "Annie Palmer",
+    title:
+      "Amazon expands its service that adds Prime badge to other sites - CNBC",
+    description:
+      "The Buy with Prime program, first announced last April, allows Prime members to check out on other retailers' sites using their Amazon account.",
+    url: "https://www.cnbc.com/2023/01/10/amazon-opens-buy-with-prime-program-to-all-sites-as-growth-slows.html",
+    urlToImage:
+      "https://image.cnbcfm.com/api/v1/image/106967796-1635521249267-gettyimages-874683314-98476223.jpeg?v=1673355601&w=1920&h=1080",
+    publishedAt: "2023-01-10T13:00:01Z",
+    content:
+      "Amazon said Tuesday it's expanding a service that lets merchants add the vaunted Prime badge to products on their own websites.\r\nThe Buy with Prime program, first announced last April, allows Prime m… [+2146 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "Yahoo Entertainment",
+    },
+    author: "Brian Sozzi",
+    title:
+      "7 whopping stats from Bed Bath & Beyond's brutal holiday quarter - Yahoo Finance",
+    description: "The outlook is bleak for Bed Bath & Beyond.",
+    url: "https://finance.yahoo.com/news/7-whopping-stats-from-bed-bath-beyonds-brutal-holiday-quarter-125451388.html",
+    urlToImage:
+      "https://s.yimg.com/ny/api/res/1.2/a5CILV32iMJfC8X8xwYBFA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD05MDA-/https://s.yimg.com/os/creatr-uploaded-images/2023-01/ace11f00-90d2-11ed-bae7-59f4773226ce",
+    publishedAt: "2023-01-10T12:54:51Z",
+    content:
+      "Bed Bath &amp; Beyond continues to struggle mightily.\r\nThe near-dead home goods retailer released its long-awaited holiday quarter results on Tuesday morning, and the picture is not pretty.\r\nHere are… [+2072 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "CNBC",
+    },
+    author: "Alex Harring, Tanaya Macheel",
+    title:
+      "Stock futures are down as traders mull prospects of higher rates - CNBC",
+    description:
+      "Stock futures were little changed Tuesday as concern over higher rates lingered among traders.",
+    url: "https://www.cnbc.com/2023/01/09/stock-market-futures-open-to-close-news.html",
+    urlToImage:
+      "https://image.cnbcfm.com/api/v1/image/107131149-1665160100518-chin.jpg?v=1673305685&w=1920&h=1080",
+    publishedAt: "2023-01-10T12:34:00Z",
+    content:
+      "Stock futures were down Tuesday as concern over higher rates lingered among traders awaiting comments from a closely followed Federal Reserve leader.\r\nFutures tied to the Dow Jones Industrial Average… [+1870 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "Fox Business",
+    },
+    author: "Gary Gastelu",
+    title:
+      "Lamborghini set another sales record in 2022 and is sold out into 2024 - Fox Business",
+    description:
+      "Lamborghini delivered a record number of cars in 2022 as demand exceeds production. The company says it has an 18-month waiting list for new orders.",
+    url: "https://www.foxbusiness.com/lifestyle/lamborghini-set-sales-record-2022",
+    urlToImage:
+      "https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2022/11/0/0/urus.jpg?ve=1&tl=1",
+    publishedAt: "2023-01-10T12:27:11Z",
+    content:
+      "Lamborghini's record sales streak continued with another big result in 2022.\r\nThe exotic performance brand delivered 9,233 vehicles worldwide, a 10% increase over its previous best set the prior year… [+1534 chars]",
+  },
+  {
+    source: {
+      id: "cnn",
+      name: "CNN",
+    },
+    author: "Nathaniel Meyersohn",
+    title: "Abercrombie & Fitch is cool again - CNN",
+    description:
+      "Abercrombie & Fitch has shed its perfume-filled mall stores, shirtless models and logo sweatshirts to win over Millennials and Gen Z.",
+    url: "https://www.cnn.com/2023/01/10/business/abercrombie-and-fitch-shopping-millennials-gen-z/index.html",
+    urlToImage:
+      "https://media.cnn.com/api/v1/images/stellar/prod/230109113322-01-abercrombie-fitch-ny-082522.jpg?c=16x9&q=w_800,c_fill",
+    publishedAt: "2023-01-10T12:17:00Z",
+    content:
+      "Abercrombie &amp; Fitch has shed its perfume-filled mall stores, shirtless models and logo sweatshirts to win over Millennials and Gen Z.\r\nIts working.\r\nAbercrombie\r\n(ANF) said Monday that its womens… [+3041 chars]",
+  },
+];
+export { newDataAPI };
